39251a436a1e89b688bd995777749fca0d82ea69
Ravi Parkash Jangra <ravijangra@gmail.com>
2020-02-15 00:07:16 +0530

    Updating the chaincode

---
+++ chaincode/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const manufacturercontract = require('./manufacturer-contract');
+//const userscontract = require('./users-contract');
+//module.exports.manufacturercontract = manufacturercontract;
+//module.exports.userscontract = userscontract;
+module.exports.contracts = [manufacturercontract];//[registrarcontract, userscontract];
